cf2cf7b5306894db15eee915de344dbf96892a1d
Newwddos <newupdateddos@hotmail.com>
2024-11-05 19:58:58 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "python3 /workspaces/laila/laila.py",
+    "postStartCommand": "python3 /workspaces/Testing/laila.py",
     "customizations": {
         "vscode": {
             "settings": {
